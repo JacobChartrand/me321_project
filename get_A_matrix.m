6e57460a9779f05643b097a55ca4d8958b8a587a
JacobChartrand <jacobchartrand99@gmail.com>
2022-06-27 16:33:13 -0400

    add matrix functions

---
+++ get_A_matrix.m
@@ -0,0 +1,24 @@
+function A = get_A_matrix(theta2,theta3, theta5, r3, r4, r5) 
+
+beta2 = theta5 - (pi/2);
+beta3 = theta2 - pi;
+beta5 = theta3 - pi;
+
+b5 = r5/2;
+
+A = [ 1 0 0 0 0 1 0 0 0 0 0 0 0;
+      0 1 0 0 0 0 1 0 0 0 0 0 0;
+      0 0 0 0 0 sin(beta2) cos(beta2) 0 0 0 0 0 1;  
+      0 0 0 0 0 1 0 cos((pi/2)+theta3) 0 0 0 0 0;
+      0 0 0 0 0 0 1 sin((pi/2)+theta3) 0 0 0 0 0;
+      0 0 -1 0 0 0 0 -(cos(theta3-(pi/2))) -1 0 0 0 0;
+      0 0 0 -1 0 0 0 -(sin(theta3-(pi/2))) 0 1 0 0 0; 
+      0 0 0 0 0 0 0 r3 -r4*sin(beta3) -r4*sin(beta3) 0 0 0;
+      0 0 0 0 0 0 0 0 -1 0 -1 0 0;
+      0 0 0 0 0 0 0 0 0 -1 0 1 0;
+      0 0 0 0 0 0 0 0 -b5*cos(beta5) b5*sin(beta5)  b5*cos(beta5) -b5*sin(beta5) 0;
+      0 0 0 0 1 0 0 0 0 0 -1 0 0;
+      0 0 0 0 0 0 0 0 0 0 0 1 0;
+    ];
+
+end
---
+++ get_ma_vector.m
@@ -0,0 +1,41 @@
+function ma = get_ma_vector(m2, m3, m4, m5, m6, theta2, dtheta2, theta3, dtheta3, ddtheta3, theta5, dtheta5, ddtheta5, r2, r3, r4, r5, ddr6, I_2, I_5)
+
+b2 = r2/2;
+b3 = r3/2;
+b5 = r5/2;
+
+I_3 = (1/3)*m3*r4^2;
+
+a_G2x = -b2*dtheta2^2*cos(theta2);
+a_G2y = -b2*dtheta2^2*sin(theta2);
+
+a_G3x = -b3*ddtheta3*sin(theta3)-b3*dtheta3^2*cos(theta3);
+a_G3y = -b3*ddtheta3*cos(theta3)-b3*dtheta3^2*sin(theta3);
+
+a_G4x = -r2*dtheta2^2*cos(theta2);
+a_G4y = -r2*dtheta2^2*sin(theta2);
+
+a_G5x = -r4*ddtheta3*sin(theta3)-r4*dtheta3^2*cos(theta3)-b5*ddtheta5 ...
+        *sin(theta5)-b5*dtheta5^2*cos(theta5);
+a_G5y = -r4*ddtheta3*cos(theta3)-r4*dtheta3^2*sin(theta3)-b5*ddtheta5 ...
+        *cos(theta5)-b5*dtheta5^2*sin(theta5);
+    
+a_6x = 0;
+a_6y = -ddr6;
+
+ma = [ m2*a_G2x;
+       m2*a_G2y;
+       0;
+       m4*a_G4x;
+       m4*a_G4y;
+       m3*a_G3x;
+       m3*a_G3y;
+       I_3*ddtheta3
+       m5*a_G5x;
+       m5*a_G5y;
+       I_5*ddtheta5;
+       0;
+       m6*a_6y;
+    ];
+
+end
